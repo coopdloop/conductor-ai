c6e6590cfdb656f2f9fe0ce440c1a48f9afcbff9
coopdloop <coopdevsec@proton.me>
2026-03-13 10:46:42 -0500

    lint:

--- src/_version.py
+++ src/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev7+g72d4525d2.d20260313'
-__version_tuple__ = version_tuple = (0, 1, 'dev7', 'g72d4525d2.d20260313')
+__version__ = version = '0.1.dev8+g922490295.d20260313'
+__version_tuple__ = version_tuple = (0, 1, 'dev8', 'g922490295.d20260313')
 
-__commit_id__ = commit_id = 'g72d4525d2'
+__commit_id__ = commit_id = 'g922490295'
